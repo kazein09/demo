2ad0dc4a89489d355044cdcd6b16a57518c06b63
Kalinux <kazein09@gmail.com>
2017-05-24 13:12:19 +0200

    new version 3

--- main.go
+++ main.go
@@ -84,10 +84,13 @@ func handleRequest(conn net.Conn) {
     fmt.Println("Error reading:", err.Error())
   }
   // Builds the message.
-  message := "Hi Dock-latest, I received your message! It was\n"
+  message := "I received your message! It was "
+  message += strconv.Itoa(reqLen)
+  message += " bytes long and that's what it said: \""
+  n := bytes.Index(buf, []byte{0})
+  message += string(buf[:n-1])
   message += "My Docker IP address is: "
   message += myHomeIP()
-  message += "\" ! Im new Update"
 
   // Write the message in the connection channel.
   conn.Write([]byte(message));
